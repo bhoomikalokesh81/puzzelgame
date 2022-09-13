7dd772d2f2cb9e7d54cc4fb6cae99e6af4efb785
bhoomikalokesh81 <101317498+bhoomikalokesh81@users.noreply.github.com>
2022-09-13 10:20:07 +0530

    Add files via upload

---
+++ puzzel game/leaderboard.js
@@ -0,0 +1,50 @@
+[
+    {
+        id: "1",
+        level: "2",
+        score: "1",
+        name: "Bhoomi",
+    },
+    {
+        id: "2",
+        level: "3",
+        score: "6",
+        name: "Bhoomi",
+    },
+    {
+        id: "3",
+        level: "4",
+        score: "60",
+        name: "Bhoomi",
+    },
+    {
+        id: "4",
+        level: "5",
+        score: "194",
+        name: "Bhoomi",
+    },
+    {
+        id: "5",
+        level: "6",
+        score: "359",
+        name: "Bhoomi",
+    },
+    {
+        id: "6",
+        level: "7",
+        score: "689",
+        name: "Bhoomi",
+    },
+    {
+        id: "7",
+        level: "8",
+        score: "1147",
+        name: "Bhoomi",
+    },
+    {
+        id: "8",
+        level: "9",
+        score: "1760",
+        name: "Bhoomi",
+    },
+];
